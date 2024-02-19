7bf67602984e124ff050f43c04e0181e9ccffc61
MusaTamzid05 <tamzid101@gmail.com>
2024-02-19 12:58:19 +0600

    Front and Back of the cube

--- include/cube.h
+++ include/cube.h
@@ -1,6 +1,34 @@
 #ifndef CUBE_H
 #define CUBE_H
+
 #include "entity.h"
+#include <vector>
+
+struct CubeMesh {
+    CubeMesh();
+    virtual ~CubeMesh();
+
+    void increase_triangle();
+    void decrease_triangle();
+
+    void update_active_vertices();
+    int get_total_draw_points() const;
+
+
+    std::vector<float> vertices;
+    std::vector<float> active_vertices;
+
+    int current_triangle_no;
+    int vertex_length;
+    int total_position_vertex_length; // number of floats that represents vertex position
+
+
+
+
+
+
+
+};
 
 struct Cube : Entity {
     Cube();
@@ -9,6 +37,9 @@ struct Cube : Entity {
     void render();
 
     unsigned int texture_id;
+    CubeMesh mesh;
+
+    int total_draw_points;
 };
 
 #endif
--- src/cube.cpp
+++ src/cube.cpp
@@ -3,77 +3,108 @@
 #include "headers.h"
 #include "camera.h"
 #include "utils.h"
-#include <vector>
+#include <iostream>
+
+CubeMesh::CubeMesh():
+    current_triangle_no(4), 
+    vertex_length(5),
+    total_position_vertex_length(3)
+{
+
+    // bottom left
+    //  top right
+    //  top left
+    vertices = {
+
+        // front
+       
+        // triangle = 1
+        -1.0f, -1.0f, 1.0f, 0.0f, 0.0f, // bottom left
+        1.0f, 1.0f, 1.0f, 1.0f, 1.0f, // top right
+        -1.0f, 1.0f, 1.0f, 0.0f, 1.0f, // top left
+
+
+        // triangle = 2
+        1.0f, -1.0f, 1.0f, 1.0f, 0.0f, // bottom right
+        1.0f, 1.0f, 1.0f, 1.0f, 1.0f, // top right
+        -1.0f, -1.0f, 1.0f, 0.0f, 0.0f, // bottom left
+
+        // back
+       
+        // triangle = 3
+        -1.0f, -1.0f, -1.0f, 1.0f, 0.0f, // bottom left
+        1.0f, 1.0f, -1.0f, 0.0f, 1.0f, // top right
+        -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, // top left
+
+
+        // triangle = 4
+        1.0f, -1.0f, -1.0f, 0.0f, 0.0f, // bottom right
+        1.0f, 1.0f, -1.0f, 0.0f, 1.0f, // top right
+        -1.0f, -1.0f, -1.0f, 1.0f, 0.0f, // bottom left
+
+    };
+
+
+
+}
+
+CubeMesh::~CubeMesh() {
+
+}
+
+void CubeMesh::increase_triangle() {
+
+}
+void CubeMesh::decrease_triangle() {
+
+}
+
+void CubeMesh::update_active_vertices() {
+    int total_values = current_triangle_no * vertex_length * total_position_vertex_length;
+    active_vertices.clear();
+
+    for(int i = 0; i < total_values; i += 1) 
+        active_vertices.push_back(vertices[i]);
+
+}
+
+
+int CubeMesh::get_total_draw_points() const {
+    return current_triangle_no * total_position_vertex_length;
+}
 
 Cube::Cube() {
 
     m_shader = new Shader("../shaders/cube.vs", "../shaders/cube.fs");
 
-	float vertices[] = {
-			-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-			 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-			 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-			 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-			-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+    mesh.update_active_vertices();
+    std::vector<float> vertices =  mesh.active_vertices;
+    std::cout << vertices.size() << "\n";
+    total_draw_points = mesh.get_total_draw_points();
+    std::cout << total_draw_points  << "\n";
 
-			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-			 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-			 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-			 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-			-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
 
-			-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-			-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-			-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    glBindVertexArray(VAO);
 
-			 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-			 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-			 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-			 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-			 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-			 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER,vertices.size() *  sizeof(float), vertices.data(), GL_STATIC_DRAW);
 
-			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-			 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-			 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-			 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // texture coord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
-			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-			 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-			 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-			 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-			-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-		};
-		glGenVertexArrays(1, &VAO);
-		glGenBuffers(1, &VBO);
+    glBindVertexArray(0);
 
-		glBindVertexArray(VAO);
+    m_shader->use();
+    glm::mat4 projection = Camera::get_instance()->get_projection();
+    m_shader->set_mat4("projection", projection);
 
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-		// position attribute
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(0);
-		// texture coord attribute
-		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-		glEnableVertexAttribArray(1);
-
-        glBindVertexArray(0);
-
-        m_shader->use();
-        glm::mat4 projection = Camera::get_instance()->get_projection();
-        m_shader->set_mat4("projection", projection);
-
-        texture_id = load_texture("../resources/number.png");
+    texture_id = load_texture("../resources/number.png");
 
 
 }
@@ -82,12 +113,15 @@ Cube::~Cube() {
 
 }
 
+
+
 void Cube::render() {
 
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture_id);
 
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0 ,36);
+    glDrawArrays(GL_TRIANGLES, 0 , total_draw_points);
+
 
 }
